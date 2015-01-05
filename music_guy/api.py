125879ff14a7f08fb12f6935e3032b098144f106
Brendon Carroll <brendonwcarroll@gmail.com>
2015-01-04 22:33:02 -0500

    get all items in library

--- music_guy/api.py
+++ music_guy/api.py
@@ -1,7 +1,24 @@
-from bottle import get
+import json
+
+from bottle import get, post
 from music_guy import db
 
 @get('/library')
 def library():
+    pass
+
+@get('/library/artists')
+def artists():
+    pass
+    # return a list of artists
+
+@get('/library/songs')
+def songs():
+    data = {}
+    data['songs'] = db.get_all()
+    return data
+
+@get('/library/songs/<songid>')
+def get_song(song_id):
     pass
     
