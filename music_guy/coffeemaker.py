65a68fed10e18ff7f1a84a60258c6ba538f7624a
Brendon Carroll <brendonwcarroll@gmail.com>
2015-01-04 22:32:11 -0500

    Added a way to include dependencies

--- music_guy/coffeemaker.py
+++ music_guy/coffeemaker.py
@@ -1,10 +1,11 @@
-"""A little module for serving coffescript files as if they were javascript
+"""A little module for CoffeScript asset management
 """
 
 import coffeescript
 import os
+import re
 
-EXTENSIONS = '.coffee'
+EXTENSIONS = ['.coffee', '.js']
 
 class Environment(object):
 
@@ -16,13 +17,65 @@ class Environment(object):
         for directory in self.directories:
             path = os.path.join(directory, name)
             if os.path.exists(path):
-                return coffeescript.compile_file(path)
+                job = Job(path)
+                return job.run()
+
+class Job(object):
+
+    def __init__(self, path):
+        self.todo = [os.path.abspath(path)]
+        self.worklist = []
+
+    def run(self):
+        while self.todo:
+            path = self.todo.pop()
+            self.preprocess(path)
+            self.worklist.insert(0, path)
+        return self.process()
+
+    def process(self):
+        compiled = []
+        for path in self.worklist:
+            if os.path.splitext(path)[1] == '.coffee':
+                compiled.append(coffeescript.compile_file(path))
+            else:
+                compiled.append(read(path))
+        return "".join(compiled)
+
+    def preprocess(self, path):
+        directory = os.path.dirname(path)
+        with open(path, 'r') as f:
+            for line in f:
+                if line[0:2] == '#=':
+                    require = re.search(r'require (.+)', line)
+                    require_dir = re.search(r'require_directory (.+)', line)
+                    if require:
+                        fpath = os.path.join(directory, require.group(1))
+                        self.require(fpath)
+                    if require_dir:
+                        dpath = os.path.join(directory, require_dir.group(1))
+                        self.require_directory(dpath)
+
+    def require(self, path):
+        if path not in self.worklist:
+            self.todo.append(path)
+
+    def require_directory(self, dirpath):
+        for root, dirs, files in os.walk(dirpath):
+            for f in files:
+                if os.path.splitext(f)[1] in EXTENSIONS:
+                    self.require(os.path.join(root, f))
+
 
 def xlate_ext(filename):
     """Translates xyz.js to xyz.coffee
     """
     name = os.path.splitext(filename)[0]
-    return name + EXTENSIONS
+    return name + '.coffee'
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return "".join(f.readlines())
 
 
 if __name__ == '__main__':
