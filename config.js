c69f1bee4f24e4e48d78804f6b8a6ac12a473773
Brendon Carroll <brendonwcarroll@gmail.com>
2015-05-06 20:38:02 -0400

    config and example config

---
+++ config.js
@@ -0,0 +1,10 @@
+
+var fs = require('fs-extra');
+
+module.exports = function config(filepath) {
+    var configString = fs.readFileSync(filepath, {
+        encoding: 'utf-8',
+    });
+    var that = JSON.parse(configString);
+    return that;
+};
